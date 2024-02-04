7a7afe7b83d7258b26c2df94b3383c6ad188e11b
raflyk <128274439+kishibee@users.noreply.github.com>
2024-02-05 00:22:04 +0700

    Time: 1510 ms (5.20%), Space: 50.3 MB (80.09%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.kt
@@ -0,0 +1,12 @@
+class Solution {
+    fun containsDuplicate(nums: IntArray): Boolean {
+        for (i in nums.indices) {
+            for (j in i+1 until nums.size) {
+                if (nums[i] == nums[j]) {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+}
