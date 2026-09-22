f67378664853db451cd41ad9357013db13414a39
raflyk <raflyade9@gmail.com>
2026-09-22 20:29:23 +0700

    Time: 0 ms (100%), Space: 4.8 MB (71.27%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.go
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -5,9 +5,6 @@ func searchInsert(nums []int, target int) int {
     }
     for i := 0; i < len(nums); i++ {
         if nums[i] > target {
-            if i == 0 {
-                return 0
-            }
             return i 
         }
         if nums[i] == target {
