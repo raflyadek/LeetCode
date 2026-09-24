9b8966b3fea3805ab8c487f9660e3b53a3299b3d
raflyk <raflyade9@gmail.com>
2026-09-24 22:05:20 +0700

    Time: 7 ms (12.22%), Space: 8.4 MB (18.16%) - LeetHub

--- 0136-single-number/0136-single-number.go
+++ 0136-single-number/0136-single-number.go
@@ -1,15 +1,12 @@
 func singleNumber(nums []int) int {
-    slices.Sort(nums)
-    lastIndex := len(nums)-1
+    mapNum := make(map[int]int) 
     for i := 0; i < len(nums); i++ {
-        if i == lastIndex {
-            return nums[lastIndex]
-        }
-        if nums[i] == nums[i+1] {
-            i += 1
-            continue
-        } else {
-            return nums[i]
+        mapNum[nums[i]]++
+    }
+    
+    for k, v := range mapNum {
+        if v == 1 {
+            return k
         }
     }
     return 0
