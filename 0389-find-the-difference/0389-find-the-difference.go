948a165d685f79e0095189efb5b4ec16c47cfba4
raflyk <raflyade9@gmail.com>
2026-09-25 13:29:21 +0700

    Time: 0 ms (100%), Space: 4.1 MB (91.4%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.go
+++ 0389-find-the-difference/0389-find-the-difference.go
@@ -1,25 +1,11 @@
 func findTheDifference(s string, t string) byte {
-    if len(s) == 0 {
-        return t[0]
-    }
-    mapByteS := make(map[byte]int)
-    mapByteT := make(map[byte]int)
-    for i := 0; i < len(t); i++ {
-        if i < len(t) - 1{
-            mapByteT[t[i]]++
-            mapByteS[s[i]]++
-            continue
-        }
-        mapByteT[t[i]]++
+    res := 0
+    for i := 0; i < len(s); i++ {
+        res = res ^ int(s[i])
     }
 
-    for k, _ := range mapByteT {
-        if _, ok := mapByteS[k]; !ok {
-            return k
-        }
-        if mapByteS[k] != mapByteT[k] {
-            return k
-        }
+    for i := 0; i < len(t); i++ {
+        res = res ^ int(t[i])
     }
-    return s[0]
+    return byte(res)
 }
